5b778f37d42d2b368c4d56449167de3ad59e49bf
Nicolas <nicolasbournas@hotmail.fr>
2024-06-21 15:47:38 +0200

    Mise en place des routes, desdossiers et des fichiers routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import UserAccount from "./pages/UserAccount";
 
-function App() {
-  
-  return (
-    <div>Hello world</div>
-  )
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/SignIn" element={<SignIn />} />
+                <Route path="/User" element={<UserAccount />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
